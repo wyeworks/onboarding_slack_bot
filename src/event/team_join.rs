010bb01dd2966a152d4b0d2627472070b2e4de62
SanGonzalezCarretero <santiagogonzalezc22@gmail.com>
2024-04-12 11:16:10 -0300

    [fix] remove 'raw' and add 'date' to Member struct

--- src/event/team_join.rs
+++ src/event/team_join.rs
@@ -5,13 +5,12 @@ use chrono::Local;
 pub fn handle_team_join(user: TeamJoinUser) {
     let timestamp = Local::now().timestamp();
 
-    let cloned_user = user.clone();
     let member = Member {
         id: user.id,
         email: user.profile.email,
         full_name: user.profile.display_name,
         country: user.tz_label.to_lowercase().replace(" time", ""),
-        _raw: cloned_user,
+        date: Local::now().format("%d-%m-%Y").to_string(),
     };
 
     let mut db = get_conn();
