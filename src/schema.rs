66502dd7b82245c9b73996ae7b33c7e33c056b00
SanGonzalezCarretero <santiagogonzalezc22@gmail.com>
2024-05-10 14:25:37 -0300

    Seeding pg_database

--- src/schema.rs
+++ src/schema.rs
@@ -5,7 +5,7 @@ diesel::table! {
         id -> Uuid,
         email -> Varchar,
         full_name -> Varchar,
-        country -> Nullable<Varchar>,
+        country -> Varchar,
         join_date -> Timestamp,
     }
 }
@@ -14,7 +14,7 @@ diesel::table! {
     onboardees (project_id, employee_id) {
         project_id -> Uuid,
         employee_id -> Uuid,
-        onboarding_date -> Nullable<Date>,
+        onboarding_date -> Date,
     }
 }
 
@@ -30,8 +30,4 @@ diesel::joinable!(onboardees -> employees (employee_id));
 diesel::joinable!(onboardees -> projects (project_id));
 diesel::joinable!(projects -> employees (admin_id));
 
-diesel::allow_tables_to_appear_in_same_query!(
-    employees,
-    onboardees,
-    projects,
-);
+diesel::allow_tables_to_appear_in_same_query!(employees, onboardees, projects,);
